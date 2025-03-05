34bbca31bf4108d2007cf118a8fb8ee0637c2112
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-05 17:29:42 +0800

    Added display name for villa properties.

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,12 @@ namespace WhiteLagoon.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
         public double Price { get; set; }
+        [Display(Name = "Square Feet")]
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image URL")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
