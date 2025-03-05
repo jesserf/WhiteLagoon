f1c5d8b09511aa476e3cf14cbf0566d4f7d434cb
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-05 17:09:43 +0800

    Initial creation of create new villa view form.

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -16,5 +16,10 @@ namespace WhiteLagoon.Web.Controllers
             var villas = _db.Villas.ToList();
             return View(villas);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
