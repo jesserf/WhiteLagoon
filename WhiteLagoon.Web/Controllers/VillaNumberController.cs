697d8be7948de825d850696d8b03f87e891323cc
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-06 15:55:11 +0800

    Added Villa room numbers module index and controller

---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+    }
+}
