3d0378c700105c89c78276ae160c022aaacb57a3
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-06 10:34:16 +0800

    Added delete and update functions in villas.

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -50,14 +50,45 @@ namespace WhiteLagoon.Web.Controllers
 
             return View(obj);
         }
+
+        [HttpPost] //identifies post endpoint
+        public IActionResult Update(Villa obj)
+        {
+            if (obj.Name == obj.Description) //checks if name and description are the same
+            {
+                ModelState.AddModelError("Description", "Name and Description cannot be the same"); //adds error message to model state
+            }
+            if (ModelState.IsValid && obj.Id>0) //checks if value aligns with data annotations
+            {
+                _db.Villas.Update(obj); //adds object to database
+                _db.SaveChanges(); //confirms insertion
+                return RedirectToAction("Index", "Villa"); //redirects to index page after insertion, (ActionName, ControllerName)
+            }
+
+            return View(); //returns view if model state is not valid
+        }
         public IActionResult Delete(int villaId)
         {
             Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
 
-            if (obj == null)
-                return NotFound();
+            if (obj is null)
+                return RedirectToAction("Error", "Home");
 
             return View(obj);
+        }
+
+        [HttpPost] //identifies post endpoint
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null) //checks if value aligns with data annotations
+            {
+                _db.Villas.Remove(objFromDb); //adds object to database
+                _db.SaveChanges(); //confirms insertion
+                return RedirectToAction("Index", "Villa"); //redirects to index page after insertion, (ActionName, ControllerName)
+            }
+
+            return View(); //returns view if model state is not valid
         }
     }
 }
