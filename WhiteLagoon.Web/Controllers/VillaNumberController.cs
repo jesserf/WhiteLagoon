df5976d88e21ad3e002e9db43b14881cde8d1122
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-06 17:03:44 +0800

    Added function of adding villa room number.

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -21,12 +22,18 @@ namespace WhiteLagoon.Web.Controllers
 
         public IActionResult Create()
         {
+            IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(u=> new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            }).ToList();
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(VillaNumber obj)
         {
+            //ModelState.Remove("Villa");
             if (ModelState.IsValid)
             {
                 _db.VillaNumbers.Add(obj);
