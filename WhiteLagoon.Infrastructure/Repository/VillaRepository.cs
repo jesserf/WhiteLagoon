61961f010a5720ab17aa716fc36f78a54aa10640
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-07 14:03:58 +0800

    Initial implementation for IVillaRepository to VillaRepository, removed Domain dependency in infrastructure

---
+++ WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        public void AddVilla(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVilla(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> GetAllVillas(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> GetVilla(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVilla(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
