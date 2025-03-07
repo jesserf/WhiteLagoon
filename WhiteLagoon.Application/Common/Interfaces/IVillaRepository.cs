df87a1598d00ae3093ffd116c27b2cf6fe33fd80
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-07 13:34:33 +0800

    Added interfaces for database context operations for Villas

---
+++ WhiteLagoon.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IVillaRepository
+    {
+        //Generic implementation to retrieve all villas
+        IEnumerable<Villa> GetAllVillas(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<Villa> GetVilla(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
+        void AddVilla(Villa entity);
+        void UpdateVilla(Villa entity);
+        void DeleteVilla(Villa entity);
+        void Save();
+    }
+}
