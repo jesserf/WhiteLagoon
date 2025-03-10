d761ee8797befd80d33dbccec15612d938af4bc2
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-10 16:17:07 +0800

    Add role management and registration enhancements
    - Created `SD.cs` with constants for user roles and removal of magic strings.
    - Updated `AccountController` to use `SD` for role checks and creation.
    - Refactored `Register` method to handle user registration asynchronously.
    - Introduced `PopulateRoleList` method to populate role selection in registration.
    - Updated links in `Login.cshtml` for proper routing.

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore.Metadata;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Web.ViewModels;
 
@@ -39,22 +40,75 @@ namespace WhiteLagoon.Web.Controllers
         }
         public IActionResult Register()
         {
-            if(!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult()) //if the role does not exist, then create the two roles, GetResult returns a boolean whilte GetAwaiter returns a task
+            if(!_roleManager.RoleExistsAsync(SD.RoleAdmin).GetAwaiter().GetResult()) //if the role does not exist, then create the two roles, GetResult returns a boolean whilte GetAwaiter returns a task
             {
-                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait(); //Will wait for the async method to finish
-                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+                _roleManager.CreateAsync(new IdentityRole(SD.RoleAdmin)).Wait(); //Will wait for the async method to finish
+                _roleManager.CreateAsync(new IdentityRole(SD.RoleCustomer)).Wait();
             }
 
-            RegisterVM registerVM = new()
-            {
-                RoleList = _roleManager.Roles.Select(x => new SelectListItem
-                {
-                    Text = x.Name,
-                    Value = x.Name
-                })
-            };
+            RegisterVM registerVM = PopulateRoleList(new RegisterVM()); //Populate the role list
 
             return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM obj)
+        {
+
+            ApplicationUser user = new()
+            {
+                Name = obj.Name,
+                Email = obj.Email,
+                PhoneNumber = obj.PhoneNumber,
+                NormalizedEmail = obj.Email.ToUpper(), //Normalized Email = Email in uppercase
+                EmailConfirmed = true,
+                UserName = obj.Email,
+                CreatedDate = DateTime.Now //When the account was created
+            };
+
+            var result = _userManager.CreateAsync(user, obj.Password).GetAwaiter().GetResult(); //Create the user with the password
+
+            if (result.Succeeded)
+            {
+                TempData["success"] = $"Account created successfully";
+                if (!string.IsNullOrEmpty(obj.Role))
+                {
+                    await _userManager.AddToRoleAsync(user, obj.Role); //Add the user to the role
+                    //AddToRolesAsync can take a list of roles e.g. IEnumerables
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, SD.RoleCustomer); //If no role is selected, add the user to the customer role
+                }
+
+                await _signInManager.SignInAsync(user, isPersistent: false); //Sign in the user
+
+                if(string.IsNullOrEmpty(obj.RedirectUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return LocalRedirect(obj.RedirectUrl);
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            TempData["error"] = $"Account could not be created";
+            obj.RoleList = PopulateRoleList(obj).RoleList; //Populate the role list
+            return View(obj);
+
+        }
+
+        private RegisterVM PopulateRoleList(RegisterVM obj)
+        {
+            obj.RoleList = _roleManager.Roles.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Name
+            });
+
+            return obj;
         }
     }
 }
