005c98575f62d73c9cd70d628d5af067d412af11
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-10 10:12:35 +0800

    Add amenity entity to project and database and seed

---
+++ WhiteLagoon.Application/Common/Interfaces/IAmenityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IAmenityRepository : IRepository<Amenity>
+    {
+        //Generic implementation to retrieve all amenities
+        void UpdateAmenity(Amenity entity);
+    }
+}
