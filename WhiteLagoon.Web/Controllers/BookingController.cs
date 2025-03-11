e5f381f5657f8a404ea1cefa9463406810ee75bc
jesserf <famorca.135737@fairview.sti.edu.ph>
2025-03-11 13:31:20 +0800

    Add booking finalization and confirmation features
    -Updated BookingController to include methods for finalizing bookings and displaying confirmation.
    -Modified `BookingConfirmation.cshtml` to show a confirmation message with the order number.
    -Enhanced FinalizeBooking.cshtml with hidden input fields for essential booking details.

--- WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System.Security.Claims;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Web.Controllers
@@ -21,21 +23,42 @@ namespace WhiteLagoon.Web.Controllers
 
             ApplicationUser user = _unitOfWork.User.Get(u => u.Id == userId); //gets application of user
 
-            
+
             Booking booking = new()
             {
                 VillaId = villaId,
-                Villa = _unitOfWork.Villa.Get(u=>u.Id == villaId, includeProperties: "VillaAmenities"),
+                Villa = _unitOfWork.Villa.Get(u => u.Id == villaId, includeProperties: "VillaAmenities"),
                 CheckInDate = checkInDate,
                 Nights = nights,
                 CheckOutDate = checkInDate.AddDays(nights),
-                UserId  = userId,
+                UserId = userId,
                 Name = user.Name,
                 Email = user.Email,
                 Phone = user.PhoneNumber
             };
             booking.TotalCost = booking.Villa.Price * nights;
             return View(booking);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult FinalizeBooking(Booking booking)
+        {
+            var villa = _unitOfWork.Villa.Get(u => u.Id == booking.VillaId); //gets Booked Villa
+            booking.TotalCost = villa.Price * booking.Nights; //gets cost according to villa price and length of stay
+            booking.Status = SD.StatusPending;
+            booking.BookingDate = DateTime.Now;
+
+            _unitOfWork.Booking.Add(booking);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
+        }
+
+        [Authorize]
+        public IActionResult BookingConfirmation(int bookingId)
+        {
+            return View(bookingId);
         }
     }
 }
